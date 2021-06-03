fb059f7cbc73bfc33daf38f2934550be1039fcd1
Nebojsa Kamber <nebojsa.kamber@4zida.rs>
2021-06-03 11:28:15 +0200

    UNDO Add CI tests (that don't need tokens)

--- tests/ZohoInvoiceServiceTest.php
+++ tests/ZohoInvoiceServiceTest.php
@@ -43,7 +43,6 @@ class ZohoInvoiceServiceTest extends TestCase
 		}
 
 	/**
-	 * @group unit
 	 * @return ZohoInvoiceService
 	 */
 	public function testInit(): ZohoInvoiceService
@@ -55,6 +54,7 @@ class ZohoInvoiceServiceTest extends TestCase
 			getenv('CLIENT_SECRET'),
 			getenv('CREDENTIALS_PATH')
 		);
+		$auth->refreshAccessToken();
 		$service = new ZohoInvoiceService(new NativeHttpClient(), $auth);
 		$this->assertNotNull($service);
 
@@ -208,7 +208,6 @@ class ZohoInvoiceServiceTest extends TestCase
 
 	/**
 	 * @depends testInit
-	 * @group unit
 	 * @throws ZohoInvoiceException
 	 */
 	public function testParseInvoiceFromWebhook(ZohoInvoiceService $service): void
